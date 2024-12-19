d3a3bf0873af8aea038c66e5ec141b01812a7392
wannnhi <nanhi@gmail.com>
2024-12-19 16:27:04 +0900

    feat : forest, flytrap Enemy

--- Assets/01.Scripts/YWH/BeetleTrigger.cs
+++ Assets/01.Scripts/YWH/BeetleTrigger.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.Rendering.Universal;
 
 public class BeetleTrigger : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class BeetleTrigger : MonoBehaviour
     [SerializeField] private Transform movepos;
     [SerializeField] private Transform originpos;
     [SerializeField] private SpriteRenderer mark;
+
+    public float markSpeed = 0.12f;
+    public float speed = 0.35f;
 
     private bool isAttacking = false; 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -18,15 +22,18 @@ public class BeetleTrigger : MonoBehaviour
             if (isAttacking) return;
 
             isAttacking = true;
-            mark.DOFade(1, 0.2f).OnComplete(() =>
+            Light2D light = mark.GetComponent<Light2D>();
+            DOTween.To(() => light.intensity, x => light.intensity = x, 1, markSpeed);
+            mark.DOFade(1, markSpeed).OnComplete(() =>
             {
-                mark.DOFade(0, 0.1f);
-                beetle.DOMoveY(movepos.position.y, 0.35f).SetDelay(0.15f)
+                mark.DOFade(0, markSpeed);
+                DOTween.To(() => light.intensity, x => light.intensity = x, 0, markSpeed);
+                beetle.DOMoveY(movepos.position.y, speed).SetDelay(0.15f)
                     .SetEase(Ease.InSine)
                     .OnComplete(() =>
                     {
 
-                        beetle.DOMoveY(originpos.position.y, 0.5f)
+                        beetle.DOMoveY(originpos.position.y, speed)
                             .SetEase(Ease.OutSine)
                             .SetDelay(1.5f)
                             .OnComplete(() =>
