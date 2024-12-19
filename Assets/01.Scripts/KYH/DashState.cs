793e59e9197f74290956bfb01d3cc36dc77b614b
fish2363 <a01022639410@gmail.com>
2024-12-19 14:22:26 +0900

    feat : ninjaBee

--- Assets/01.Scripts/KYH/DashState.cs
+++ Assets/01.Scripts/KYH/DashState.cs
@@ -64,7 +64,7 @@ public class DashState : State
             _player.isUndead = true;
             AnimationPlayer.Instance.PlayAnimaiton(_player.AnimatorCompo, "FatTired");
             _player.SpriteCompo.DOFade(0.3f, 0.2f);
-            _player.RigidCompo.velocity = Vector2.down * _player.DashPower;
+            _player.RigidCompo.velocity = Vector2.down * _player.DashPower/2;
             if(_player.CurrentHp != _player.MaxHp)
                 _player.CurrentHp++;
             if (dashTime >= maxaDashTime)
