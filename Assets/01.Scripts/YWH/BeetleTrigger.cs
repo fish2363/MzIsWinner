43d4a90c75ecb01b7495ffa6b1f40ccf96737bb6
wannnhi <nanhi@gmail.com>
2024-12-19 14:21:21 +0900

    feat : beetle

--- Assets/01.Scripts/YWH/BeetleTrigger.cs
+++ Assets/01.Scripts/YWH/BeetleTrigger.cs
@@ -8,22 +8,33 @@ public class BeetleTrigger : MonoBehaviour
     [SerializeField] private Transform beetle;
     [SerializeField] private Transform movepos;
     [SerializeField] private Transform originpos;
+    [SerializeField] private SpriteRenderer mark;
 
-    private bool isAttaking;
+    private bool isAttacking = false; 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            if (isAttaking)
+            if (isAttacking) return;
+
+            isAttacking = true;
+            mark.DOFade(1, 0.2f).OnComplete(() =>
             {
-                return;
-            }
+                mark.DOFade(0, 0.1f);
+                beetle.DOMoveY(movepos.position.y, 0.35f).SetDelay(0.15f)
+                    .SetEase(Ease.InSine)
+                    .OnComplete(() =>
+                    {
 
-            isAttaking = true;
-
-            beetle.DOMoveY(movepos.position.y, 0.25f).SetEase(Ease.InSine).OnComplete(() =>
-            { beetle.DOMoveY(originpos.position.y, 0.5f).SetEase(Ease.OutSine).SetDelay(2).OnComplete(() => { isAttaking = false; }); });
+                        beetle.DOMoveY(originpos.position.y, 0.5f)
+                            .SetEase(Ease.OutSine)
+                            .SetDelay(1.5f)
+                            .OnComplete(() =>
+                            {
+                                isAttacking = false;
+                            });
+                    });
+            });
         }
-        
-   }
+    }
 }
