88385f065ff537a6490e16dc1cbcdd75fc7fc02b
fish2363 <a01022639410@gmail.com>
2024-12-20 08:16:17 +0900

    ASDA

--- Assets/01.Scripts/MSM/Boss/Frog/Frog.cs
+++ Assets/01.Scripts/MSM/Boss/Frog/Frog.cs
@@ -160,7 +160,7 @@ public class Frog : MonoBehaviour,IBoss
         }
         GameManager.Instance.FadeIn();
         ScreenShakeManager.Instance.ScreenShake(0f, true, 100, true, 1f);
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(5f);
         Time.timeScale = 1f;
         GameManager.Instance.NextStage();
     }
