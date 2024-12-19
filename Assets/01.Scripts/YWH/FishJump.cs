d0f29843f4d03c316d6d21d4ca8987d26a8ce70a
wannnhi <nanhi@gmail.com>
2024-12-20 02:15:19 +0900

    feat : many change

--- Assets/01.Scripts/YWH/FishJump.cs
+++ Assets/01.Scripts/YWH/FishJump.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using DG.Tweening;
+using UnityEngine.Rendering.Universal;
 
 public class FishMovement : MonoBehaviour
 {
@@ -8,19 +9,24 @@ public class FishMovement : MonoBehaviour
     public Transform targetPoint;
     public float heightMin = 2f;
     public float heightMax = 5f;
+    public float playerDetectionRadius = 5f; // 플레이어 감지 범위
 
     private bool isMovingToTarget = true;
     private SpriteRenderer sprite;
     private Vector3 originalScale;
     private int attackDamage = 1;
+    private SpriteRenderer mark;
+    private Transform player;
 
     private void Awake()
     {
         SpawnManager.Instance.OnSpawn += FindPlayer;
     }
+
     public void FindPlayer()
     {
         sprite = GetComponent<SpriteRenderer>();
+        mark = startPoint.GetChild(0).GetComponent<SpriteRenderer>();
         originalScale = transform.localScale;
         StartCoroutine(MoveFish());
     }
@@ -29,15 +35,32 @@ public class FishMovement : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(Random.Range(0, 3));
+            
+            while (!CheckForPlayer())
+            {
+                yield return new WaitForSeconds(0.5f); 
+            }
+
+            yield return new WaitForSeconds(Random.Range(0, 1));
             Transform from = isMovingToTarget ? startPoint : targetPoint;
             Transform to = isMovingToTarget ? targetPoint : startPoint;
 
             float height = Random.Range(heightMin, heightMax);
-            yield return StartCoroutine(ParabolicMove(from.position, to.position, height, 1.5f));
+            yield return StartCoroutine(ParabolicMove(from.position, to.position, height, 3f));
             isMovingToTarget = !isMovingToTarget;
             yield return new WaitForSeconds(Random.Range(1, 5));
         }
+    }
+
+    private bool CheckForPlayer()
+    {
+        Collider2D playerCollider = Physics2D.OverlapCircle(transform.position, playerDetectionRadius, LayerMask.GetMask("Player"));
+        if (playerCollider != null)
+        {
+            player = playerCollider.transform; 
+            return true;
+        }
+        return false;
     }
 
     private IEnumerator ParabolicMove(Vector3 start, Vector3 end, float height, float duration)
@@ -45,12 +68,21 @@ public class FishMovement : MonoBehaviour
         float time = 0;
         while (time < duration)
         {
+            Light2D light = mark.GetComponent<Light2D>();
+            DOTween.To(() => light.intensity, x => light.intensity = x, 1, 0.12f);
+            mark.DOFade(1, 0.12f).OnComplete(() =>
+            {
+                mark.DOFade(0, 0.12f);
+                DOTween.To(() => light.intensity, x => light.intensity = x, 0, 0.12f);
+            });
             float t = time / duration;
             Vector3 linearPosition = Vector3.Lerp(start, end, t);
             float parabolicHeight = Mathf.Sin(Mathf.PI * t) * height;
             transform.position = new Vector3(linearPosition.x, linearPosition.y + parabolicHeight, linearPosition.z);
             time += Time.deltaTime;
             yield return null;
+
+            
         }
         transform.position = end;
     }
@@ -76,5 +108,12 @@ public class FishMovement : MonoBehaviour
     private void OnDisable()
     {
         SpawnManager.Instance.OnSpawn -= FindPlayer;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, playerDetectionRadius);
     }
 }
